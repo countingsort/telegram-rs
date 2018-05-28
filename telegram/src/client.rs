cda02dba2cdc9396d92747347fab11e3032b5e74
countingsort <niclas@countingsort.com>
2018-05-28 23:01:29 +0200

    Made this usuable
    - simpler-client-api
    - deserialization with serde

--- telegram/src/client.rs
+++ telegram/src/client.rs
@@ -1,48 +1,51 @@
-use futures::{future, Stream, Future};
-use hyper::{self, Body};
+use futures::{Future, Stream};
 use hyper::client::HttpConnector;
+use hyper::{self, Body};
 use serde::Serialize;
 use serde::de::DeserializeOwned;
 use serde_mtproto::Identifiable;
-use tokio::reactor::Handle;
+use tokio::reactor::Core;
 
 use error;
 use request::Request;
 use response::Response;
 
 pub struct Client {
+    core: Core,
     http_client: hyper::Client<HttpConnector, Body>,
 }
 
 impl Client {
     /// Create a new Telegram client.
     #[inline]
-    pub fn new(handle: &Handle) -> Client {
-        Client {
-            http_client: hyper::Client::new(handle),
-        }
+    pub fn new() -> error::Result<Client> {
+        let core = Core::new()?;
+        let http_client = hyper::Client::new(&core.handle());
+
+        Ok(Client {
+            core: core,
+            http_client: http_client,
+        })
     }
 
     // Send a constructed request using this Client.
-    pub fn send<T, U>(
-        &self,
-        req: Request<T>
-    ) -> Box<Future<Item = Response<U>, Error = error::Error>>
-        where T: Serialize + Identifiable,
-              U: 'static + DeserializeOwned + Identifiable,
+    pub fn send<F, T, U, R>(&mut self, req: Request<T>, on_receive_handler: F) -> error::Result<R>
+    where
+        F: FnOnce(Response<U>) -> R,
+        T: Serialize + Identifiable,
+        U: 'static + DeserializeOwned + Identifiable,
     {
-        let http_request = match req.to_http_request() {
-            Ok(req) => req,
-            Err(error) => return Box::new(future::err(error)),
-        };
+        let http_request = req.to_http_request()?;
 
-        Box::new(
+        let promise = Box::new(
             self.http_client
                 .request(http_request)
                 .and_then(|res| res.body().concat2())
                 .map(|data| Response::from_reader(&*data))
                 .flatten()
-                .map_err(|err| err.into())
-        )
+                .map_err(|err| err.into()),
+        ).map(on_receive_handler);
+
+        self.core.run(promise)
     }
 }
